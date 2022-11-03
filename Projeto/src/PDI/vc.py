0e9477277c66ec51eb23b873ac95b7ef8a598a94
Breno Sampaio <br.sampaio@outlook.com>
2022-11-03 14:01:37 -0300

    implementação de função
    implementada as funções:
    
    1 - Carregar imagem random
    2 - Carregar uma imagem conhecida
    3 - Escrever sobre a imagem

--- Projeto/src/PDI/vc.py
+++ Projeto/src/PDI/vc.py
@@ -3,10 +3,10 @@ import numpy as np
 
 def cut_img(imagem, l, a):
     '''
-    :param imagem: variável que contém a imagem
-    :param l: largura desejada para o recorte
-    :param a: altura desejada para o recorte
-    :return: recorte da imagem
+    :param imagem: variável que contém a imagem.
+    :param l: largura desejada para o recorte.
+    :param a: altura desejada para o recorte.
+    :return: recorte da imagem com seu centro no centro da imagem original.
     '''
     (largura, altura, canais) = np.shape(imagem)
     (c_x, c_y) = (largura//2, altura//2)
--- Projeto/src/utils/utl.py
+++ Projeto/src/utils/utl.py
@@ -1,13 +1,22 @@
 import cv2
 import matplotlib.pyplot as plt
+import os
+import random
 
 
-def show_img(titulo, img, a = 6.4, b = 4.8):
+def show_img(titulo, img, l = 6, a = 6):
+    '''
+    :param titulo: Escrever o título como string
+    :param img: variável que contém a imagem
+    :param a: largura desejada
+    :param b: altura desejada
+    :return: visualização da imagem desejada.
+    '''
     try:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     except:
         pass
-    plt.figure(figsize=(a, b))
+    plt.figure(figsize=(l, a))
     plt.title(titulo)
     plt.imshow(img, cmap='gray')
     plt.show()
@@ -31,3 +40,76 @@ def escrever(img, texto, cor=(255,0,0)):
     '''
     fonte = cv2.FONT_HERSHEY_SIMPLEX 
     cv2.putText(img, texto, (10,20), fonte, 0.5, cor, 0, cv2.LINE_AA)
+
+
+def load_random_img():
+    '''
+    :return: retorna o par (nome da imagem, imagem) a imagem é aleatória.
+    '''
+    # Caminhos para os DataSets
+    path_dataSet_benigno = "..\\..\\DataSet\\all_nods_benignos"
+    path_dataSet_maligno = "..\\..\\DataSet\\all_nods_malignos"
+
+
+    #Montando as listas com os nomes dos arquivos
+    for root, dirs, files in os.walk(path_dataSet_benigno):
+        dataset_benigno = [file for file in files]
+
+    for root, dirs, files in os.walk(path_dataSet_maligno):
+        dataset_maligno = [file for file in files]
+
+    # lista com o nome de todos os arquivos
+    dataset = sorted(dataset_benigno + dataset_maligno)
+
+    #Selecionando uma imagem qualquer
+    img_selecionada = random.choice(dataset)
+    print(f"{img_selecionada} - Nome da imagem random gerada.")
+
+    #Definindo o caminho para uso da função cv2.read() e posterior mostra da imagem.
+    if img_selecionada in dataset_benigno:
+        path = os.path.join(path_dataSet_benigno, img_selecionada)
+        img = cv2.imread(path)
+    else:
+        path = os.path.join(path_dataSet_maligno, img_selecionada)
+        img = cv2.imread(path)
+
+    # Passando para cinza
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    return (f"{img_selecionada}", img_gray)
+
+
+def load_img(name_img):
+    '''
+    :param name_img: Nome da imagem no dataset com extensão.
+    :return: carrega a imagem desejada já em cinza.
+    '''
+    # Caminhos para os DataSets
+    path_dataSet_benigno = "..\\..\\DataSet\\all_nods_benignos"
+    path_dataSet_maligno = "..\\..\\DataSet\\all_nods_malignos"
+
+    # Montando as listas com os nomes dos arquivos
+    for root, dirs, files in os.walk(path_dataSet_benigno):
+        dataset_benigno = [file for file in files]
+
+    for root, dirs, files in os.walk(path_dataSet_maligno):
+        dataset_maligno = [file for file in files]
+
+    # lista com o nome de todos os arquivos
+    dataset = sorted(dataset_benigno + dataset_maligno)
+
+    # Selecionando uma imagem qualquer
+    img_selecionada = name_img
+
+    # Definindo o caminho para uso da função cv2.read() e posterior mostra da imagem.
+    if img_selecionada in dataset_benigno:
+        path = os.path.join(path_dataSet_benigno, img_selecionada)
+        img = cv2.imread(path)
+    else:
+        path = os.path.join(path_dataSet_maligno, img_selecionada)
+        img = cv2.imread(path)
+
+    # Passando para cinza
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    return img_gray
